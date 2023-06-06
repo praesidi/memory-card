1d301543eb27bd32249dc70fdd0ff2d7ea2c3c11
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-06-06 22:12:02 +0400

    chore: export images for cards
    Export images to be able to import them in 1 line

---
+++ src/assets/images/index.js
@@ -0,0 +1,53 @@
+import belaf from './belaf.jpeg';
+import bondrewd from './bondrewd.jpeg';
+import faputa from './faputa.jpeg';
+import gaburoon from './gaburoon.jpg';
+import gyarike from './gyarike.jpg';
+import habo from './habo.jpeg';
+import jiruo from './jiruo.jpeg';
+import lyza from './lyza.jpeg';
+import maaa from './maaa.jpg';
+import majikaja from './majikaja.jpeg';
+import marulk from './marulk.jpeg';
+import mio from './mio.jpeg';
+import mitty from './mitty.jpeg';
+import moogie from './moogie.jpeg';
+import nanachi from './nanachi.jpeg';
+import nat from './nat.jpeg';
+import ouzen from './ouzen.jpeg';
+import prushka from './prushka.jpeg';
+import reg from './reg.jpg';
+import riko from './riko.jpg';
+import shiggy from './shiggy.jpeg';
+import srajo from './srajo.jpeg';
+import vuelo from './vuelo.jpeg';
+import wazukyan from './wazukyan.jpeg';
+
+const images = {
+	belaf,
+	bondrewd,
+	faputa,
+	gaburoon,
+	gyarike,
+	habo,
+	jiruo,
+	lyza,
+	maaa,
+	majikaja,
+	marulk,
+	mio,
+	mitty,
+	moogie,
+	nanachi,
+	nat,
+	ouzen,
+	prushka,
+	reg,
+	riko,
+	shiggy,
+	srajo,
+	vuelo,
+	wazukyan,
+};
+
+export default images;
