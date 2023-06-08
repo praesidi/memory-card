aa72f2f5ec1f061ca3892fdd51bd45e9bb88167d
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-06-08 22:57:26 +0400

    chore: change tailwind font size values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+		fontSize: {
+			sm: '0.8rem',
+			base: '1rem',
+			xl: '1.25rem',
+			'2xl': '1.5rem',
+			'3xl': '1.85rem',
+			'4xl': '2.4rem',
+			'5xl': '3rem',
+		},
+	},
+	plugins: [],
 };
