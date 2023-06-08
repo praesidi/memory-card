62eaf37fbc978db1795684db1b6bff32b97ff375
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-06-08 22:54:00 +0400

    style: change score board's margin for better ux

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -6,7 +6,7 @@ export default function ScoreBoard({
 	bestScore: number;
 }) {
 	return (
-		<div className='flex justify-evenly text-4xl text-yellow-500 '>
+		<div className='flex justify-evenly w-full mt-10 text-4xl text-yellow-500 '>
 			<span className='block'>Score: {score}</span>
 			<span className='block'>Best Score: {bestScore}</span>
 		</div>
