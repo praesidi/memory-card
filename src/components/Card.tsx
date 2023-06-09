355fcec997039f01c5f397d5112fe590463afbe1
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-06-09 17:38:26 +0400

    refactor: clear the code a bit

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,9 +12,9 @@ export default function Card({
 	return (
 		<div
 			className='justify-self-center flex flex-col justify-end w-[180px] rounded-xl relative text-center text-2xl capitalize text-yellow-500 overflow-hidden hover:scale-105 transition-all duration-200 hover:drop-shadow-[0_0_15px_rgba(255,255,255,0.5)]'
-			onClick={onClick}
+			onClick={() => onClick(name)}
 		>
-			<img src={imageUrl} className='' alt='card image' />
+			<img src={imageUrl} alt='card image' />
 			<span className='absolute w-full bg-black/50'>{name}</span>
 		</div>
 	);
