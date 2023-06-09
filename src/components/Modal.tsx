4c4b0a6dcb60f67f752ca836f05f9cf542f9e982
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-06-09 18:58:05 +0400

    refactor: rewrite titles and delete unnecessary lines

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useRef } from 'react';
-
 export default function Modal({
 	result,
 	isOpen,
@@ -15,36 +13,54 @@ export default function Modal({
 		flexDirection: 'column',
 		justifyContent: 'space-evenly',
 		alignItems: 'center',
-		position: 'fixed',
-		top: '50%',
-		left: '50%',
-		// transform: isOpen ? 'translate(-50%, -50%)' : 'translate(-50%, -150%)',
-		transform: 'translate(-50%, -50%)',
+		left: '0',
+		right: '0',
+		top: '0',
+		bottom: '0',
+		margin: 'auto',
+		position: 'absolute',
 		width: '520px',
 		height: '340px',
 		boxSizing: 'border-box',
 		padding: '20px',
-		backgroundColor: 'rgba(235, 235, 235, 0.97)',
+		backgroundColor: 'rgba(235, 235, 235, 0.9)',
 		zIndex: '999',
 		boxShadow: '0 0 16px rgba(0,0,0,0.5)',
 		transition: 'all 0.3s ease-out',
 	};
 
+	const overlayStyle = {
+		position: 'fixed',
+		top: '0',
+		left: '0',
+		right: '0',
+		bottom: '0',
+		width: '100vw',
+		height: '100%',
+		backgroundColor: 'rgba(0,0,0,0.6)',
+		zIndex: '998',
+		backdropFilter: 'blur(3px)',
+		transition: 'all 0.3s ease-out',
+	};
+
 	return (
-		<dialog className='clip-modal' style={modalStyle} open>
-			{result === 'win' ? (
-				<h1 className='text-green-600 text-4xl'>You Won!</h1>
-			) : (
-				<h1 className='text-red-500 text-4xl'>You Lost!</h1>
-			)}
-			<button
-				className='text-yellow-500 text-2xl py-3 px-4 uppercase bg-[#4d330a] 
-				hover:scale-105 transition-all duration-200 hover:drop-shadow-[0_0_5px_rgba(0,0,0,0.3)]
-				active:scale-100 clip-button'
-				onClick={onRestart}
-			>
-				restart
-			</button>
-		</dialog>
+		<>
+			<dialog className='clip-modal' style={modalStyle} open>
+				{result === 'win' ? (
+					<h1 className='text-green-600 text-4xl'>You Win!</h1>
+				) : (
+					<h1 className='text-red-500 text-4xl'>You Lose!</h1>
+				)}
+				<button
+					className='text-yellow-500 text-2xl py-3 px-4 uppercase bg-[#4d330a] 
+					hover:scale-105 transition-all duration-200 hover:drop-shadow-[0_0_5px_rgba(0,0,0,0.3)]
+					active:scale-100 clip-button'
+					onClick={onRestart}
+				>
+					restart
+				</button>
+			</dialog>
+			<div style={overlayStyle}></div>
+		</>
 	);
 }
