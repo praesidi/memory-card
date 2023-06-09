c93f2d1a0f97ec7f217a55f96bcac9c566be7fa6
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-06-09 21:59:22 +0400

    feat: make the page more responsive

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -10,7 +10,14 @@ export default function Container({
 	const imageKeys = Object.keys(images).sort(() => Math.random() - 0.5);
 
 	return (
-		<div className='grid grid-rows-3 grid-cols-6 gap-6 w-10/12 my-24'>
+		<div
+			className='grid gap-6 w-10/12 my-24
+				2xl:grid-rows-3 2xl:grid-cols-6
+				xl:grid-rows-4 xl:grid-cols-5
+				lg:grid-rows-5 lg:grid-cols-4
+				md:grid-rows-6 md:grid-cols-3
+				sm:grid-rows-9 sm:grid-cols-2 '
+		>
 			{imageKeys.map((key) => (
 				<Card
 					key={key}
