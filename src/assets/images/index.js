52b38e9ff69efc4afdb60136fa48c25b71072396
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-06-09 17:37:10 +0400

    refactor: delete unused images for the cards

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -8,18 +8,12 @@ import jiruo from './jiruo.jpeg';
 import lyza from './lyza.jpeg';
 import maaa from './maaa.jpg';
 import majikaja from './majikaja.jpeg';
-import marulk from './marulk.jpeg';
-import mio from './mio.jpeg';
-import mitty from './mitty.jpeg';
 import moogie from './moogie.jpeg';
 import nanachi from './nanachi.jpeg';
-import nat from './nat.jpeg';
 import ouzen from './ouzen.jpeg';
 import prushka from './prushka.jpeg';
 import reg from './reg.jpg';
 import riko from './riko.jpg';
-import shiggy from './shiggy.jpeg';
-import srajo from './srajo.jpeg';
 import vuelo from './vuelo.jpeg';
 import wazukyan from './wazukyan.jpeg';
 
@@ -34,18 +28,12 @@ const images = {
 	lyza,
 	maaa,
 	majikaja,
-	marulk,
-	mio,
-	mitty,
 	moogie,
 	nanachi,
-	nat,
 	ouzen,
 	prushka,
 	reg,
 	riko,
-	shiggy,
-	srajo,
 	vuelo,
 	wazukyan,
 };
